7b701e4e8f6eb5d56cbae68f572ac2bf3f079cf2
eliesat <112230228+eliesat@users.noreply.github.com>
2025-03-06 12:45:55 +0200

    Add files via upload

--- allineone/all/_scripts-update.sh
+++ allineone/all/_scripts-update.sh
@@ -1,9 +1,37 @@
 #!/bin/sh
 
+# Functions
+###########################################
+print_message() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
+}
+print_message "> Start of process ..."
+echo "-----------------------------------------------"
+echo
+sleep 2
+
+cleanup() {
+    rm -rf /var/cache/opkg/* /var/lib/opkg/lists/* /run/opkg.lock $i >/dev/null 2>&1
+}
+
+#check print image and python version
+###########################################
+if [ -f /etc/image-version ]; then image_version=$(cat /etc/image-version | grep -iF "creator" | cut -d"=" -f2 | xargs) 
+elif [ -f /etc/issue ]; then image_version=$(cat /etc/issue | head -n1 | awk '{print $1;}') 
+else 
+image='> image name not found' 
+fi 
+
+python_version=$(python -c "import platform; print(platform.python_version())")
+
+print_message "> Image : $image_version"
+sleep 2
+print_message "> Python : $python_version"
+sleep 2
+
+
 # Download and install scripts
 ###########################################
-if [ ! -f /usr/script/Eliesat-Eliesatpanel.sh ] ; then
-
 plugin=main
 version='scripts'
 url=https://github.com/eliesat/scripts/archive/main.tar.gz
@@ -24,11 +52,8 @@ if [ $extract -eq 0 ]; then
 print_message "> Eliesatscripts are installed successfully and up to date ..."
 sleep 3
 fi
-fi
 
 print_message "> End of process ..."
 sleep 3
-
-print_message "> Please Wait enigma2 restarting. ..."
+echo
 echo "-----------------------------------------------------------"
-sleep 3
